27c2e395c550c39f5d40d02f71b6a51a0cb365ae
kavyashre <65527764+kavyashre@users.noreply.github.com>
2020-05-27 11:38:38 +0530

    Add files via upload

---
+++ kavyashree m_ 23 may(code challenge).py
@@ -0,0 +1,8 @@
+Printf(“ enter correct username and password to continue ‘); Count=0
+
+While count<4:
+Username =input(‘enter username:	‘) Password=input(‘enter password:		‘)
+If	username==’Micheal’ and password==’e3$WT89x’: Printf( ‘access granted’ )
+Count=10
+Else
+Printf(‘ access denied.try again later ’) Count+=1
